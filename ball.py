9883a8a0ddb6cf5233f1f8488f4e99f6b9edc18f
OlaPietka <36880623+OlaPietka@users.noreply.github.com>
2020-05-20 21:00:01 +0200

    Add graphics and smole changes

--- ball.py
+++ ball.py
@@ -4,12 +4,17 @@ import numpy as np
 class Ball:
     ball_mass = 0.5
     friction = 0.9
+    size = 20
+    radius = size / 2
 
     def __init__(self):
-        self.pos = np.zeros(2, dtype=float)
+        self.pos = np.array([100, 100], dtype=float)
         self.vel = np.zeros(2, dtype=float)
+        self.image = 'images/ball_golf.png'
 
     def update_pos(self, force, t):
         self.vel += (force / self.ball_mass) * t
         self.vel *= self.friction
         self.pos += self.vel * t
+
+
--- main.py
+++ main.py
@@ -20,6 +20,17 @@ c = 10
 
 ti = time.time()
 
+
+def get_image(obj):
+    image = pygame.image.load(obj.image)
+    image = pygame.transform.scale(image, (obj.size, obj.size))
+    return image
+
+
+def center_pos(obj):
+    return obj.pos - obj.radius
+
+
 while not game_end:
     screen.fill(0)
 
@@ -39,12 +50,12 @@ while not game_end:
             force = np.array([xi - xf, yi - yf]) * c
 
     ball.update_pos(force, dt)
-    if ball.pos[0] + 25 >= 600 or ball.pos[0] - 25 <= 0:
+    if ball.pos[0] + ball.radius >= 600 or ball.pos[0] - ball.radius <= 0:
         ball.vel[0] *= -1
-    if ball.pos[1] + 25 >= 600 or ball.pos[1] - 25 <= 0:
+    if ball.pos[1] + ball.radius >= 600 or ball.pos[1] - ball.radius <= 0:
         ball.vel[1] *= -1
 
-    pygame.draw.circle(screen, (0, 0, 255), np.array(ball.pos, dtype=int), 25)
+    screen.blit(get_image(ball), np.array(center_pos(ball), dtype=int))
     pygame.display.flip()
 
     clock.tick(10)
